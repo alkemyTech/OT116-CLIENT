edc4f9a0c336206458e22b006b91c946ad9995d7
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2021-12-16 23:21:52 -0300

    changed styles implementation of campaign header

--- src/Components/CampaignHeader/CampaignHeader.jsx
+++ src/Components/CampaignHeader/CampaignHeader.jsx
@@ -1,52 +1,12 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
 import ONGLogo from '../../Assets/Logo/logo.png';
+import './CampaignHeader.css';
 
 const CampaignHeader = ({ campaignLogo, slogan }) => (
   <header role="menu">
-    <Box
-      sx={{
-        paddingBlock: 2,
-        display: 'flex',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-        maxHeight: '120px',
-        background: '#ff79614f',
-      }}
-    >
-      <img src={campaignLogo} style={{ maxWidth: '80px' }} alt="campaignLogo" />
-      <Typography
-        sx={{
-          color: '#DB5752',
-          letterSpacing: 2,
-          fontWeight: 'bold',
-          display: {
-            xs: 'none',
-            sm: 'none',
-            md: 'block',
-            lg: 'block',
-            xl: 'block',
-          },
-        }}
-        variant="h4"
-        component="h4"
-      >
-        {slogan}
-      </Typography>
-      <Box
-        sx={{
-          display: {
-            xs: 'none',
-            sm: 'block',
-            md: 'block',
-            lg: 'block',
-            xl: 'block',
-          },
-        }}
-      >
-        <img src={ONGLogo} style={{ maxWidth: '100px' }} alt="ONG Logo" />
-      </Box>
-    </Box>
+    <img className="campaign_logo" src={campaignLogo} alt="campaignLogo" />
+    <h1 className="campaign_slogan">{slogan}</h1>
+    <img className="ong_logo" src={ONGLogo} alt="ONG Logo" />
   </header>
 );
 export default CampaignHeader;
