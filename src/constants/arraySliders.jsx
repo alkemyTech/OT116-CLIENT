57e4dab7778521b65c4c633cf884dc2cc3f9d201
Beto Fiorani <betofiorani@gmail.com>
2021-12-18 01:48:36 -0300

    OT116-78 - Reuse Slider Component - Init & Config

--- src/constants/arraySliders.jsx
+++ src/constants/arraySliders.jsx
@@ -1,6 +1,9 @@
 import sliderJuguetes1 from '../img/sliderJuguetes1.jpg';
 import sliderJuguetes2 from '../img/sliderJuguetes2.jpg';
 import sliderJuguetes3 from '../img/sliderJuguetes3.jpg';
+import sliderEscolar1 from '../img/sliderEscolar1.jpg';
+import sliderEscolar2 from '../img/sliderEscolar2.jpg';
+import sliderEscolar3 from '../img/sliderEscolar3.jpg';
 
 const arraySlides = [
   {
@@ -24,6 +27,33 @@ const arraySlides = [
   {
     name: 'slide4',
     url: sliderJuguetes1,
+    text: 'texto al pie',
+    targetUrl: 'https://www.ole.com.ar',
+  },
+];
+
+export const arraySlidesEscolar = [
+  {
+    name: 'slide1',
+    url: sliderEscolar1,
+    text: 'texto al pie',
+    targetUrl: 'https://www.ole.com.ar',
+  },
+  {
+    name: 'slide2',
+    url: sliderEscolar2,
+    text: 'texto al pie',
+    targetUrl: 'https://www.ole.com.ar',
+  },
+  {
+    name: 'slide3',
+    url: sliderEscolar3,
+    text: 'texto al pie',
+    targetUrl: 'https://www.ole.com.ar',
+  },
+  {
+    name: 'slide4',
+    url: sliderEscolar1,
     text: 'texto al pie',
     targetUrl: 'https://www.ole.com.ar',
   },
