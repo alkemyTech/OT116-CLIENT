5e535cc569c81054cd397a08584df154b990c083
anaferreira <estudioj.ferreira@gmail.com>
2021-12-21 21:42:45 -0300

    Refactor debounce function

--- src/Components/Search/NovedadesSearch.jsx
+++ src/Components/Search/NovedadesSearch.jsx
@@ -8,15 +8,14 @@ const NovedadesSearch = function () {
   const [searchedTitle, setSearchedTitle] = useState('');
   const filter = data.filter((item) => item.color.includes(searchedTitle));
 
-  const handleInput = (val) => {
-    const dbo = debounce(() => setSearchedTitle(val), 2000);
-    dbo();
-  };
+  const handleInput = debounce((val) => {
+    setSearchedTitle(val);
+  }, 2000);
 
   return (
     <div className="search">
       <input type="text" placeholder="Buscar titulo" className="search-input" onChange={(e) => handleInput(e.target.value)} />
-      {searchedTitle.length > 0 && <div>{filter.map((i) => <p>{i.color}</p>)}</div>}
+      {searchedTitle.length > 0 && <div>{filter.map((i) => <p key={i}>{i.color}</p>)}</div>}
     </div>
   );
 };
