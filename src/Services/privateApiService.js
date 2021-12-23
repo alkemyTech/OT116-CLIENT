9c48ee96eddc8a6db3bc0cd292c7e07a66a29d3c
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2021-12-22 22:35:13 -0300

    added getPrivate method

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,7 +1,17 @@
-export default function getHeaderAuthorization() {
+import axios from 'axios';
+
+export function getHeaderAuthorization() {
   const isTokenSaved = window.localStorage.getItem('token');
 
   return {
     Authorization: `Bearer ${isTokenSaved}`,
   };
+}
+
+export async function getPrivate(url, id) {
+  const targetURL = `${url}/${id}`;
+  const request = await axios.get(targetURL, {
+    headers: getHeaderAuthorization(),
+  });
+  return request.data;
 }
