7ef499a93d3b1f1db5ef5eb3a8949b3e2563efd3
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2021-12-23 13:25:46 -0300

    improved getHeaderAuthorization method

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,6 +1,7 @@
 export default function getHeaderAuthorization() {
   const isTokenSaved = window.localStorage.getItem('token');
 
+  if (!isTokenSaved) return {};
   return {
     Authorization: `Bearer ${isTokenSaved}`,
   };
