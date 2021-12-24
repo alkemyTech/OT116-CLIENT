33165ae6fb03d3213233cb7cf73c1dbd1e0d1404
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2021-12-23 23:44:11 -0300

    added patchPrivateById method

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -9,10 +9,18 @@ export default function getHeaderAuthorization() {
   };
 }
 
+const headers = {
+  ...getHeaderAuthorization(),
+};
+
 export async function getPrivateById(url, id) {
   const targetURL = id ? `${url}/${id}` : url;
-  const request = await axios.get(targetURL, {
-    headers: getHeaderAuthorization(),
-  });
+  const request = await axios.get(targetURL, headers);
+  return request.data.data;
+}
+
+export async function patchPrivateById(url, id, body) {
+  const targetURL = `${url}/${id}`;
+  const request = await axios.patch(targetURL, body, headers);
   return request.data.data;
 }
