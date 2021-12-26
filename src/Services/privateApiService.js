0da70dd0039b1e310f522f41091e791c5ab73d45
Beto Fiorani <betofiorani@gmail.com>
2021-12-26 14:14:06 -0300

    OT116-27 - Reusable Post Private endPoint

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default function getHeaderAuthorization() {
   const isTokenSaved = window.localStorage.getItem('token');
 
@@ -5,4 +7,19 @@ export default function getHeaderAuthorization() {
   return {
     Authorization: `Bearer ${isTokenSaved}`,
   };
+}
+
+const baseUrl = 'http://ongapi.alkemy.org/api/'; // import from the right place.
+const headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+  'access-control-allow-methods': 'GET, PUT, POST, DELETE, OPTIONS',
+  Authorization: getHeaderAuthorization(),
+}; // import headers from the right palce
+
+export async function postPrivateEndPoint(path, data) {
+  const request = await axios.post(baseUrl + path, data, {
+    headers,
+  });
+  return request.data; // return request or request.data.....
 }
