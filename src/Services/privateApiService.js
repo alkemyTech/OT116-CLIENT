7da85c190c6300cced03d501ae33b4b5dc9d833a
Juan Manuel Sanchez Diaz <73983475+jmsanchezdiaz@users.noreply.github.com>
2021-12-28 23:01:24 -0300

    OT116-26 - HTTP Private Services - Get Header Authorization Object Method (#16)
    * added getHeaderAuthorization method
    
    * added getPrivateById method
    
    * added patchPrivateById method

---
+++ src/Services/privateApiService.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export default function getHeaderAuthorization() {
+  const isTokenSaved = window.localStorage.getItem('token');
+
+  if (!isTokenSaved) return {};
+  return {
+    Authorization: `Bearer ${isTokenSaved}`,
+  };
+}
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+  'access-control-allow-methods': 'GET, PUT, PATCH, POST, DELETE, OPTIONS',
+  ...getHeaderAuthorization(),
+};
+
+export async function getPrivateById(url, id) {
+  const targetURL = `${url}/${id}`;
+  const request = await axios.get(targetURL, headers);
+  return request.data.data;
+}
+
+export async function patchPrivateById(url, id, body) {
+  const targetURL = `${url}/${id}`;
+  const request = await axios.patch(targetURL, body, headers);
+  return request.data.data;
+}
