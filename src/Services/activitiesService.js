e95cba2add4209cb9e36c31ed7e63c8f8d033607
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2022-01-04 09:59:23 -0300

    added post, and delete methods

--- src/Services/activitiesService.js
+++ src/Services/activitiesService.js
@@ -1,4 +1,8 @@
-import { getPrivateById, patchPrivateById } from './privateApiService';
+import {
+  deletePrivateEndPointById,
+  getPrivateById, patchPrivateById,
+  postPrivateEndPoint,
+} from './privateApiService';
 import { getEndpoint } from '../Components/Services/publicApiServices';
 
 const baseURL = 'http://ongapi.alkemy.org/api/activities';
@@ -19,9 +23,25 @@ export const getActivityById = async (id) => {
   }
 };
 
-export const updateActivityById = async (id, updatedActivity) => {
+export const patchActivityById = async (id, updatedActivity) => {
   try {
     return await patchPrivateById(baseURL, id, updatedActivity);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const addActivity = async (data) => {
+  try {
+    return await postPrivateEndPoint('activities', data);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const deleteActivity = async (id) => {
+  try {
+    return await deletePrivateEndPointById('activities', id);
   } catch (error) {
     throw new Error(error);
   }
