d45db7d9ed61296a1dfe4dde01f2f7d96772e40a
Beto Fiorani <betofiorani@gmail.com>
2022-01-06 01:44:50 -0300

    OT116-59 - Slides Reducer + Actions + Add in Store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import slidesReducer from './slidesReducer/slidesReducer';
 import userReducer from './usersReducer/userReducer';
 
 const store = configureStore({
   reducer: {
     users: userReducer,
+    slides: slidesReducer,
   },
 });
 
