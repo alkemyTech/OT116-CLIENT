61afdb5afe671bf0944868ce204d29f1e6c5ed5f
anaferreira <estudioj.ferreira@gmail.com>
2022-01-05 21:10:11 -0300

    Added form validation and endpoint connection

--- src/Components/Search/NovedadesSearch.jsx
+++ src/Components/Search/NovedadesSearch.jsx
@@ -2,20 +2,36 @@ import React, { useState } from 'react';
 import { debounce } from 'lodash';
 import './NovedadesSearch.css';
 
-const data = [{ color: 'white' }, { color: 'blue' }, { color: 'red' }, { color: 'pink' }];
-
 const NovedadesSearch = function () {
   const [searchedTitle, setSearchedTitle] = useState('');
-  const filter = data.filter((item) => item.color.includes(searchedTitle));
+  const [message, setMessage] = useState('');
 
   const handleInput = debounce((val) => {
-    setSearchedTitle(val);
-  }, 2000);
+    if (val.length >= 3) {
+      fetch(`http://ongapi.alkemy.org/api/news?search=${val}`)
+        .then((res) => res.json())
+        .then((json) => {
+          setSearchedTitle(json.data);
+          setMessage('No hay resultados para tu búsqueda');
+        });
+    } else {
+      setMessage('Ingresa al menos 3 caracteres para la búsqueda');
+    }
+  }, 1000);
 
   return (
     <div className="search">
-      <input type="text" placeholder="Buscar titulo" className="search-input" onChange={(e) => handleInput(e.target.value)} />
-      {searchedTitle.length > 0 && <div>{filter.map((i) => <p key={i}>{i.color}</p>)}</div>}
+      <input
+        type="text"
+        placeholder="Buscar titulo"
+        className="search-input"
+        onChange={(e) => handleInput(e.target.value)}
+      />
+      {searchedTitle.length > 0 ? (
+        <div>{searchedTitle.map((i) => <p key={i}>{i.name}</p>)}</div>
+      ) : (
+        <p>{message}</p>
+      )}
     </div>
   );
 };
