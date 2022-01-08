3e20db44b9697b2ea0db05bc1bdc04440545c195
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2022-01-07 23:37:38 -0300

    added authReducer and reducers, services FIXES

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import slidesReducer from './slidesReducer/slidesReducer';
 import userReducer from './usersReducer/userReducer';
 import activitiesReducer from './activitiesReducer/activitiesReducer';
+import authReducer from './authReducer/authReducer';
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     users: userReducer,
     slides: slidesReducer,
     activities: activitiesReducer,
