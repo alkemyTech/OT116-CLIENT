1d76c37dedbd49b97ce9dc737541a2676f8c6fb4
GonzaloVolonterio <volonterio.gonzalo1987@gmail.com>
2022-01-09 21:25:46 -0300

    add OT116-147

--- src/Components/Spinner/Spinner.jsx
+++ src/Components/Spinner/Spinner.jsx
@@ -1,32 +1,11 @@
-/* eslint-disable jsx-quotes */
 import React from 'react';
-// import {useState, useEffect} from 'react';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 const Spinner = function () {
-  // const [spinner, setSpinner] = useState(true);
-  // useEffect{() => {
-  // setLoading(true),
-  // setTimeout(()=> {
-  // setLoading(false)
-  // },8000)
-  // },[]}
-
-  // { loading ? (
-  // <Loader
-  //  type='BallTriangle'
-  //  color='#00BFFF'
-  //  height={80}
-  //  width={80}
-  // )
-  //  : (
-  //  <div className="pagina que quiera cargar"></div>
-  // )
-  // }
   return (
-    <div className="loader">
-      <Loader type="BallTriangle" color="#00BFFF" height={120} width={120} />
+    <div className="justify-content-center align-items-center">
+      <Loader type="BallTriangle" color="#00BFFF" height={100} width={100} />
     </div>
   );
 };
