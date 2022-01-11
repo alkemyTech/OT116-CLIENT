8aa109a3c19ff0ff248f6a98c0f94079a55c0a21
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2022-01-11 19:15:55 -0300

    documentation added

--- src/Components/ActivitiesSearch/ActivitiesSearch.test.js
+++ src/Components/ActivitiesSearch/ActivitiesSearch.test.js
@@ -1,87 +1,121 @@
-import { screen, render, act, cleanup, fireEvent, waitFor} from '@testing-library/react';
-import ActivitiesSearch, { apiCalls }  from './ActivitiesSearch';
-import { getAllActivities, searchActivityByTitle} from '../../Services/activitiesService';
+import {
+  screen,
+  render,
+  act,
+  cleanup,
+  fireEvent,
+  waitFor,
+} from '@testing-library/react';
+import ActivitiesSearch, { apiCalls } from './ActivitiesSearch';
 import userEvent from '@testing-library/user-event';
 
 describe('<ActivitiesSearch /> Testing', () => {
-
-  jest.mock("axios");
-
   let component;
 
   beforeEach(() => {
-    component = render (<ActivitiesSearch />);
-  })
+    component = render(<ActivitiesSearch />);
+  });
 
   afterEach(() => cleanup);
 
-  it('should render input with empty value',  () => {
-    expect(screen.getByTestId('input')).toBeInTheDocument();
-    expect(screen.getByTestId('input').value).toBeFalsy()
-  })
-
-  it('should input value change on typing', () => {
-    const input = screen.getByTestId('input');
-    const inputWord = 'chs'
-    act(() => {
-      userEvent.type(input, inputWord);
+  describe('<Input /> Tests', () => {
+    it('should render input with empty value', () => {
+      // I expect to the input element to be render in the document.
+      expect(screen.getByTestId('input')).toBeInTheDocument();
+      // Also expect to the input element value to be empty at start (i use toBeFalsy() because a empty string is consider a falsy value).
+      expect(screen.getByTestId('input').value).toBeFalsy();
     });
 
-    expect(input.value).toBe(inputWord)
-  })
+    it('should input value change on typing', () => {
+      const input = screen.getByTestId('input'); // I save the input element
+      const inputWord = 'chs'; // I save the input word
+      act(() => {
+        userEvent.type(input, inputWord); // Simulate user typing inputWord
+      });
 
-  it('should helper text show a initial text', () => {
-    const helperText = screen.getByTestId('helperText');
-    const initialText = 'Porfavor Introduzca una Actividad';
+      expect(input.value).toBe(inputWord); // I expect to input value to be inputWord.
+    });
+  });
 
-    expect(helperText.textContent).toBe(initialText);
-  })
+  describe('<FormHelperText /> Test', () => {
+    it('should helper text show a initial text', () => {
+      const helperText = screen.getByTestId('helperText'); // i save the helperText element
+      const initialText = 'Porfavor Introduzca una Actividad'; // i save the initial text i expect to helperText element have.
 
-  it('when input value characters are less or equal than 3 helper text shows a message', async () => {
-    const input = screen.getByTestId('input');
-    act(() => {
-      userEvent.type(input, 'chs');
+      expect(helperText.textContent).toBe(initialText); // I expect the helperText element text content to be equal to the value saved at initial text.
     });
 
-    await waitFor(() => expect(screen.getByTestId('helperText').textContent).toBe('Ingrese más de 3 caracteres'));
+    it('when input value characters are less or equal than 3 helper text shows a message', async () => {
+      const input = screen.getByTestId('input'); // i save the input element
+      const expectedText = 'Ingrese más de 3 caracteres'; // i save the expected text
+      act(() => {
+        userEvent.type(input, 'chs'); // Simulate user typing "chs"
+      });
 
-  })
-
-  it('when input value characters are more than 3 helper text shows a message', async () => {
-    const input = screen.getByTestId('input');
-    act(() => {
-      userEvent.type(input, 'chss');
+      await waitFor(() =>
+      // Expect that text content of helper text element to be expectedText
+        expect(screen.getByTestId('helperText').textContent).toBe(expectedText),
+      );
     });
 
-    await waitFor(() => expect(screen.getByTestId('helperText').textContent).toBe('0 resultados con titulo: chss'));
-  })
+    it('when input value characters are more than 3 helper text shows a message', async () => {
+      const input = screen.getByTestId('input'); // i save the input element
+      const inputWord = 'chss'; // i save the input word
+      const expectedText = `0 resultados con titulo: ${inputWord}`; // i save the expected text;
 
-  it('should call api method getAllActivities when input change and input value characters are less than 3', async () => {
-    const input = screen.getByTestId('input');
-    const mockedGetActivitiesFunction = jest.spyOn(apiCalls, 'getAllActivities');
-    act(() => {
-      userEvent.type(input, 'chs');
+      act(() => {
+        userEvent.type(input, inputWord); // Simulate user typing inputWord
+      });
+
+      await waitFor(() =>
+      // Expect that text content of helper text element to be expectedText
+        expect(screen.getByTestId('helperText').textContent).toBe(expectedText)
+      );
     });
-    
-    await waitFor(() => expect(mockedGetActivitiesFunction).toHaveBeenCalledTimes(1));
-    await waitFor(() => expect(screen.queryByTestId('results-list')).toBeInTheDocument());
-  })
+  });
 
-  it('should call api method searchActivitiyByTitle when input change and input value characters are more than 3', async () => {
-    const input = screen.getByTestId('input');
-    const inputWord = 'taller de manualidades';
-    const mockedSearchFunction = jest.spyOn(apiCalls, 'searchResults');
+  describe('API Calls Test', () => {
+    it('should call api method getAllActivities when input change and input value characters are less than 3', async () => {
+      const input = screen.getByTestId('input'); //  I save the input.
+      const mockedGetActivitiesFunction = jest.spyOn(
+        apiCalls,
+        'getAllActivities',
+      ); //  I tell to jest to spy the function getAllActivities from apiCalls object and save it at mockedGetActivitiesFunction
 
-    act(() => {
-      userEvent.type(input, inputWord);
+      act(() => {
+        userEvent.type(input, 'chs'); // Simulate user typing "chs"
+      });
+
+      await waitFor(() =>
+        // I wait for the debounce time and then expect to mockedGetActivitiesFunction to have been called once
+        expect(mockedGetActivitiesFunction).toHaveBeenCalledTimes(1),
+      ); 
+      await waitFor(() =>
+        // I wait for the the debounce time as well and expect to results list to be render in the document.  
+        expect(screen.queryByTestId('results-list')).toBeInTheDocument(),
+      ); 
     });
 
-    expect(input.value).toBe(inputWord);
-    await waitFor(() => {
-      expect(mockedSearchFunction).toHaveBeenCalledTimes(1)
-      expect(mockedSearchFunction).toHaveBeenCalledWith(inputWord)
-    });
-    await waitFor(() => expect(screen.queryByTestId('results-list')).toBeInTheDocument());
+    it('should call api method searchActivitiyByTitle when input change and input value characters are more than 3', async () => {
+      const input = screen.getByTestId('input'); //  I save the input.
+      const inputWord = 'taller de manualidades'; //  I save the input word.
+      const mockedSearchFunction = jest.spyOn(apiCalls, 'searchResults'); //  I tell to jest to spy the function searchResults from apiCalls object and save it at mockedSearchFunction
 
-  })
-})
+      act(() => {
+        userEvent.type(input, inputWord); // Simulate user typing inputWord
+      });
+
+      await waitFor(() => {
+        // I wait for the debounce time and then expect to mockedSearchResultsFunction to have been called once
+        expect(mockedSearchFunction).toHaveBeenCalledTimes(1); 
+        //And expect to mockedSearchResultsFunction to have been with inputWord as parameter
+        expect(mockedSearchFunction).toHaveBeenCalledWith(inputWord);
+      });
+
+      await waitFor(() => {
+       // I wait for the debounce time as well and expect to results list to be render in the document.  
+        expect(screen.queryByTestId('results-list')).toBeInTheDocument();
+      })
+    });
+  });
+});
