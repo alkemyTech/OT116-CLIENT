f8be734b189b4b47a6d33ac0090806d58a5192a7
vsol <vsol.sil@gmail.com>
2022-01-13 22:18:52 -0300

    login form acces only for no authenticated users

--- src/Components/Users/Login.jsx
+++ src/Components/Users/Login.jsx
@@ -1,38 +1,54 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { login } from '../../app/authReducer/authActions';
-import { logout } from '../../app/authReducer/authReducer';
+
+const schema = Yup.object().shape({
+  email: Yup.string().email('El correo electrónico es inválido').required('Este campo es obligatorio'),
+  password: Yup.string().required('Este campo es obligatorio'),
+});
 
 const Login = () => {
   const dispatch = useDispatch();
+
   const {
-    status, error, isAuthenticated, user,
+    error, isAuthenticated,
   } = useSelector((state) => state.auth);
-  const [values, setValues] = useState({
-    email: '',
-    password: '',
-  });
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    return setValues({
-      ...values,
-      [e.target.name]: e.target.value,
-    });
-  };
+  const { push } = useHistory();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(login(values));
-  };
+  useEffect(() => {
+    if (isAuthenticated) push('/');
+  }, [isAuthenticated]);
+
   return (
     <div>
-      { !isAuthenticated
-        ? (
+      <h2>Iniciar Sesión</h2>
+      <Formik
+        validationSchema={schema}
+        validateOnBlur
+        onSubmit={(data) => {
+          dispatch(login(data));
+        }}
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+      >
+        {({
+          handleSubmit,
+          handleChange,
+          handleBlur,
+          values,
+          touched,
+          errors,
+        }) => (
           <Box
             component="form"
             sx={{
@@ -49,9 +65,12 @@ const Login = () => {
                 name="email"
                 value={values.email}
                 onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.email && errors.email}
                 defaultValue="ejemplo@ejemplo.com"
                 helperText="Ingresa tu correo electrónico"
               />
+              <div type="invalid">{errors.email}</div>
               <TextField
                 id="outlined-password-input"
                 label="*******"
@@ -59,25 +78,17 @@ const Login = () => {
                 name="password"
                 value={values.password}
                 onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.password && errors.password}
                 autoComplete="current-password"
                 helperText="Ingresa tu contraseña"
               />
+              <div type="invalid">{errors.password}</div>
             </div>
             <Button type="submit" variant="contained" onClick={handleSubmit}>LogIn</Button>
           </Box>
-        )
-        : (
-          <div>
-            <h3>Usuario Logueado</h3>
-            <p>
-              Estado:
-              {' '}
-              {status}
-            </p>
-            <p>{user.name}</p>
-            <Button type="button" variant="contained" onClick={() => dispatch(logout())}>LogOut</Button>
-          </div>
         )}
+      </Formik>
       {
         error && <p>{error}</p>
       }
