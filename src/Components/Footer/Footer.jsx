f119f6b81d608756c61502a3daeeefcc2e1760ea
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2022-01-15 18:58:42 -0300

    added non responsive home footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,17 +1,20 @@
 import { Container, TableFooter } from '@mui/material';
+import { Link } from 'react-router-dom';
 import React from 'react';
+import ONGLogo from '../../Assets/Logo/logo.png';
+import './Footer.css';
 
 const Footer = function () {
   return (
     <>
       <div className="ContenedorWaveFooter" />
       <TableFooter
+        as="footer"
         sx={{
           display: { xs: 'flex' },
           justifyContent: { xs: 'center' },
           alignItems: { xs: 'center' },
           flexDirection: { xs: 'column' },
-          backgroundColor: '#28527A',
           color: 'white',
         }}
       >
@@ -19,11 +22,15 @@ const Footer = function () {
           sx={{
             display: { xs: 'grid', sm: 'flex' },
             justifyContent: { xs: 'center' },
+            gap: '2rem',
             alignItems: { xs: 'center' },
           }}
         >
-          Footer
+          <Link className="footerLink" to="/toys-campaign">Campaña de Jugutes</Link>
+          <Link to="/"><img src={ONGLogo} alt="somos mas logo" /></Link>
+          <Link className="footerLink" to="/school-campaign">Campaña Escolar</Link>
         </Container>
+        <p className="copyright">2021 by Alkemy. All Rights Reserved.</p>
       </TableFooter>
     </>
   );
