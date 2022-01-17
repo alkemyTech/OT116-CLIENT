c50a8f73eaf5bc653f897327781d72fffd0e3a0d
vsol <vsol.sil@gmail.com>
2022-01-17 20:50:02 -0300

    changing slide object attributes

--- src/Components/Slide/Slide.jsx
+++ src/Components/Slide/Slide.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 const Slide = function ({ slide, config }) {
   return (
     <div className={`slide-container ${config.customClasses.slideContainer}`}>
-      <a href={slide.targetUrl}>
-        <img className={`slider-image ${config.customClasses.slideImage}`} src={slide.url} alt={slide.name} />
-      </a>
+      <img className={`slider-image ${config.customClasses.slideImage}`} src={slide.image} alt={slide.name} />
       <div className="slider-text-container">
-        <p className={`slider-text ${config.customClasses.slideText}`}>{slide.text}</p>
+        <p className={`slider-text ${config.customClasses.slideText}`}>{slide.description}</p>
       </div>
     </div>
   );
